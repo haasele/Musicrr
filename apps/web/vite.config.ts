130e330b55382fc1d750580c2206b1dadc9d2d96
haasele <admin@haasele.org>
2026-04-27 02:25:16 +0200

    tls

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -13,7 +13,6 @@ export default defineConfig({
   server: {
     port: 3000,
     host: true,
-    https: {},
-    allowedHosts: ["musicrr.haasele.dev"]
+    allowedHosts: ["musicrr.haasele.dev", ".haasele.dev"]
   }
 });
